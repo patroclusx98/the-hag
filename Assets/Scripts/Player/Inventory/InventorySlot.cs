fc17b45eeff6006d5f102a626e9bdbd87eeaf0a2
Kristof Sotet <kristof.sotet@morrisonsplc.co.uk>
2023-09-30 21:56:24 +0100

    fix: inventory slot bug

--- Assets/Scripts/Player/Inventory/InventorySlot.cs
+++ Assets/Scripts/Player/Inventory/InventorySlot.cs
@@ -33,9 +33,8 @@ public class InventorySlot : MonoBehaviour
             else
             {
                 PlayerInventory.instance.selectedItem = item;
+                GetComponentInParent<InventoryUI>().ToggleInventory(true);
             }
-
-            GetComponentInParent<InventoryUI>().ToggleInventory(true);
         }
     }
 }
