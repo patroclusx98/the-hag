54a7351b5e0f66e9338349b77c9b8195b51430c0
Kristof Sotet <kristof.sotet@morrisonsplc.co.uk>
2023-10-01 10:55:06 +0100

    refactor: change frequency timer to object timer

---
+++ Assets/Scripts/ObjectTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectTimer : MonoBehaviour
+{
+    private string id;
+    private float timeCounter;
+
+    /// <summary>
+    /// Attaches a timer with a unique ID to the defined game object and starts it
+    /// </summary>
+    /// <param name="id">Unique ID of this timer instance</param>
+    /// <param name="gameObject">Game object to attach the timer to</param>
+    /// <param name="time">Time in seconds to set the timer to</param>
+    /// <returns>True on timer start, False while timer is progressing</returns>
+    public static bool StartTimer(string id, GameObject gameObject, float time)
+    {
+        string prefixedId = gameObject.name + "_" + id;
+        ObjectTimer objectTimer = Array.Find(gameObject.GetComponents<ObjectTimer>(), (objectTimer) => objectTimer.id == prefixedId);
+
+        if (objectTimer == null)
+        {
+            objectTimer = gameObject.AddComponent<ObjectTimer>();
+            objectTimer.id = prefixedId;
+        }
+
+        if (objectTimer.timeCounter == 0f)
+        {
+            objectTimer.timeCounter = time;
+            return true;
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (timeCounter > 0f)
+        {
+            timeCounter -= Time.deltaTime;
+
+            if (timeCounter <= 0f)
+            {
+                timeCounter = 0f;
+            }
+        }
+    }
+}
